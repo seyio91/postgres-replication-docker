eab317dd49a1c7fb85f9f1e9318d4b43c9b56dbc
seyio91 <seyi_obaweya@yahoo.com>
2020-05-09 10:40:43 +0200

    created cleanup file

--- cleanup.sh
+++ cleanup.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 echo "Stopping Slave Container"
+docker stop replica-db
 
 echo "Stopping Master"
+docker stop master-db
 
 echo "Deleting Network"
+docker network rm test
 
 echo "deleting master volume"
 sudo rm -rf volumes/*
 
-docker-compose rm -f
+echo "Deleting Docker Images"
+docker rmi replication-slave
+docker rmi replication-master
